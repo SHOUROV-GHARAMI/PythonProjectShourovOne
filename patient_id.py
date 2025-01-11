3dd6fc898ee9c041b1be18a2557d7e8015b9c788
SHOUROV-GHARAMI <shourovgharami222@gmail.com>
2025-01-11 19:37:26 +0600

    summation

---
+++ patient_id.py
@@ -0,0 +1,19 @@
+patient_name = input( 'Patient name: ')
+birth_year = input('Birth year: ')
+age = 2025 -int (birth_year)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(patient_name)
+print(age)
---
+++ summation.py
@@ -0,0 +1,4 @@
+first = input("Enter the first mumber: ")
+second = input("Enter the second number: ")
+sum = float(first) + float(second)
+print("Total: ", sum)
