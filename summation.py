ae7e1d8706eaff81674245ba9db16c46bbaa65b6
SHOUROV-GHARAMI <shourovgharami222@gmail.com>
2025-01-12 01:32:56 +0600

    Operators

--- summation.py
+++ summation.py
@@ -1,4 +1,17 @@
-first = input("Enter the first mumber: ")
-second = input("Enter the second number: ")
-sum = float(first) + float(second)
-print("Total: ", sum)
+first = float(input("Enter the first mumber: "))
+second = float(input("Enter the second number: "))
+add = first + second
+sub = first - second
+mul = first * second
+div = first / second
+modulus = first % second
+exponent = first ** second
+floor_division = first // second
+
+print("\nAddition of those two numbers is: ", add)
+print("\n Subtraction of those two numbers is: ", sub)
+print("\n Multiplication of those two numbers is: ", mul)
+print("\n Division of those two numbers is: ", div)
+print("\n Modulus of those two numbers is: ", modulus)
+print("\n Exponent of those two numbers is: ", exponent)
+print("\n Floor Division of those two numbers is: ", floor_division)
